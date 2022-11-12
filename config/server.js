4dd388c69edcc476e3c3f8afce20e0dc1bb0c0e8
SIMO-99 <coccasimone1999@gmail.com>
2022-11-12 11:53:32 +0100

    renamed database folder into config. added server default config file

---
+++ config/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+module.exports = app;
