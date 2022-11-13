0b59d125e71cc8a4210b82b2c7ff0b93d0a74b5a
SIMO-99 <coccasimone1999@gmail.com>
2022-11-13 17:10:41 +0100

    solved a problem that could stop the server when the month has no submits into the db

--- controllers/month.js
+++ controllers/month.js
@@ -21,11 +21,11 @@ exports.pastMonths = async function (req, res) {
 exports.getMonth = async (req, res) => {
 
     var returned = {};
-    if (req.query.y == null) { res.sendStatus(404); return; }
+    if (req.query.y == null) { res.sendStatus(400); return; }
 
     const submits = await s.query("select month, year, submits.id, resources, new_resources, points, new_points, trophies, new_trophies, name, color, player_id FROM submits inner join players on players.id=submits.player_id where month=" + req.params.month + " and year=" + req.query.y + " order by name", { type: sequelize.QueryTypes.SELECT });
 
-    if (submits.length < 1) { res.sendStatus(404); return; }
+    if (submits.length < 1) { res.sendStatus(204); return; }
 
     console.log(req.url);
 
