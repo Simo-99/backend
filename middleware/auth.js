48acfaa31752ef6212fcf8609d4ed08577de31c5
SIMO-99 <coccasimone1999@gmail.com>
2022-11-22 15:21:04 +0100

    removed a log

--- middleware/auth.js
+++ middleware/auth.js
@@ -35,7 +35,6 @@ exports.helper = async (req, res, next) => {
 
     const auth = req.headers["authorization"];
     const token = auth && auth.split(" ")[1];
-    console.log(token)
     if (token == null) return res.sendStatus(401);
 
     const token_db = await Token.findOne({ where: { token: token }, include: { model: User, as: "user" } });
