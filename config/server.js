8b95398f3197ec87ac3b8b963d1d425e78f92b50
SIMO-99 <coccasimone1999@gmail.com>
2023-01-01 18:18:16 +0100

    changed cors options

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
